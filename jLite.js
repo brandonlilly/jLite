fe5fb224f8e705f696401802e16cb9d7367c9cee
Brandon Lilly <brandonjlilly@gmail.com>
2015-05-11 18:23:44 -0700

    initial

---
+++ jLite.js
@@ -0,0 +1,75 @@
+var LCollection = function (els) {
+  this.els = els || [];
+};
+
+LCollection.prototype.each = function (callback) {
+  this.els.forEach(function (el) {
+    callback(el);
+  });
+};
+
+LCollection.prototype.first = function () {
+  return new LCollection([this.els[0]]);
+};
+
+LCollection.prototype.last = function () {
+  return new LCollection([this.els[this.els.length - 1]]);
+};
+
+LCollection.prototype.html = function (text) {
+  if (text) {
+    this.each(function (el) {
+      el.innerHTML = text;
+    });
+    return this;
+  }
+  return this.els[0].innerHTML
+};
+
+LCollection.prototype.val = function (value) {
+  return this.attr('value', value);
+};
+
+LCollection.prototype.attr = function (attribute, value) {
+  if(typeof value !== 'undefined') {
+    this.each(function (el) {
+      el.setAttribute(attribute, value);
+    });
+    return this;
+  }
+  return this.els[0].getAttribute(attribute);
+};
+
+LCollection.prototype.append = function (node) {
+  if(node instanceof String) {
+    console.log('not implemented yet');
+  }
+  this.each(function (el) {
+    el.appendChild(node);
+  });
+  return this;
+};
+
+
+L = function(selector) {
+  if(selector instanceof HTMLElement) {
+    return new LCollection([selector]);
+  }
+  if(selector instanceof Array) {
+    return new LCollection(selector)
+  }
+
+  var els = []
+  switch(selector.charAt(0)) {
+    case '.':
+      els = document.getElementsByClassName(selector.substr(1));
+      break;
+    case '#':
+      els = [document.getElementById(selector.substr(1))];
+      break;
+    default:
+      els = document.getElementsByTagName(selector);
+      break;
+  }
+  return new LCollection(els)
+};
